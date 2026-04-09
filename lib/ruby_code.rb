91cf7f116c6b94329838179e442be76ae2787201
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2026-04-09 00:15:03 -0600

    Remove unused recue

--- lib/ruby_code.rb
+++ lib/ruby_code.rb
@@ -4,11 +4,9 @@ require_relative "ruby_code/version"
 require_relative "ruby_code/config/user_config"
 require_relative "ruby_code/auth/auth_manager"
 require_relative "ruby_code/initializer"
+require_relative "ruby_code/plugins"
 
-begin
-  raise "This gem requires Ruby 3.2.0 or higher" if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("3.2.0")
-rescue LoadError
-end
+raise "This gem requires Ruby 3.2.0 or higher" if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("3.2.0")
 
 # Main module for the RubyCode gem
 module RubyCode
@@ -16,5 +14,3 @@ module RubyCode
     Initializer.new
   end
 end
-
-require_relative "ruby_code/plugins"
